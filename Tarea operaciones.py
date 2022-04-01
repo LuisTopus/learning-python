f7c2113a0380632c461425882b2f09a779525f27
Luisocvilla <topus_@hotmail.com>
2022-03-31 23:08:32 -0600

    guardando tarea

---
+++ Tarea operaciones.py
@@ -0,0 +1,95 @@
+
+
+print("Hola soy zeus y te ayudare a sumar restar multipilcar o dividir")
+print ("Lo se mi poder es basto")
+print ("Que operacion quieres realizar")
+
+
+print ("Escribe la operacion que realizaras sumar, restar, multiplicar, dividir")
+
+_1 = "sumar"
+_2 = "restar"
+_3 = "multiplicar"
+_4 = "dividir"
+
+
+accion = str(input())
+
+if accion == _1:
+    print ("Ingresa un numero para sumar")
+    numero1 = int (input())
+
+    print("Ingresa el segundo numero")
+    numero2 = int  (input())
+
+    lasuma = numero1 + numero2
+    print ("la suma es:",lasuma)
+ 
+
+if accion ==_2:
+
+        print("Ingresa un numero para restar")
+        numero1 = int (input())
+
+        print ("ingresa el otro numero a restar")
+        numero2 = int (input())
+        laresta = numero1 - numero2
+        print ("la resta es:",laresta)
+
+if accion ==_3:
+        print("Ingresa un numero para multiplicar")
+        numero1 = int (input())
+
+        print ("ingresa el otro numero a multiplicar")
+        numero2 = int (input())
+        lamultiplicacion = numero1 * numero2
+        print ("la multiplicacion es:",lamultiplicacion)
+
+if accion ==_4:
+        
+        print("Ingresa un numero para dividir")
+        numero1 = int (input())
+
+        print ("ingresa el otro numero a dividir")
+        numero2 = int (input())
+        
+        ladivision = numero1 / numero2
+        print ("la division es:",ladivision)
+
+else:
+        print ("no sabes escribir")
+
+        
+        
+        
+
+        
+
+        
+
+    
+    
+    
+    
+
+    
+
+            
+            
+
+    
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
