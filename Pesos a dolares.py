255d5723746085be406718947dec1f66ff831a45
Luisocvilla <topus_@hotmail.com>
2022-03-31 20:07:03 -0600

    subiendo archivo dolares

---
+++ Pesos a dolares.py
@@ -0,0 +1,11 @@
+
+print ("Hola te ayudare a convertir los pesos en dolares")
+
+print ("cuantos pesos quieres convertir a dolares")
+pesos = int (input())
+
+
+pesos / 19.90
+print (pesos / 19.90)
+
+
